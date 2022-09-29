482a2408806f63441750d25c64d30a797785c38c
Sanjog Wanem <sjwanem@gmail.com>
2022-09-29 08:11:21 +0100

    Comments

--- Meal-Maker.js
+++ Meal-Maker.js
@@ -1,3 +1,9 @@
+/*
+  A restaurant has hired you to create a function for their website that allows them to set a meal and price each morning for Today’s Special. 
+  Use your knowledge of getters and setters to make sure anyone who uses the new function can generate a meal and a price for Today’s Special 
+  without any embarrassing errors!
+*/
+
 const menu = {
     _meal: '',
     _price: 0,
